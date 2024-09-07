503625950ba7785b2ed79bf612c35cc4e0a2f4f7
Chimney2 <damian.dubicki@gmail.com>
2024-09-07 17:35:20 -0400

    first commit

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def predict():
     
     return jsonify({'artwork': predicted_label})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=5000, debug=False)
